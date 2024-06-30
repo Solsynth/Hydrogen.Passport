659fc8375c43d96ba33ca7fcbe0ef7dc3d459efc
LittleSheep <littlesheep.code@hotmail.com>
2024-06-30 11:41:33 +0800

    :bug: Fix request body validation

--- pkg/internal/server/admin/notify_api.go
+++ pkg/internal/server/admin/notify_api.go
@@ -11,16 +11,13 @@ import (
 
 func notifyAllUser(c *fiber.Ctx) error {
 	var data struct {
-		ClientID     string                    `json:"client_id" validate:"required"`
-		ClientSecret string                    `json:"client_secret" validate:"required"`
-		Type         string                    `json:"type" validate:"required"`
-		Subject      string                    `json:"subject" validate:"required,max=1024"`
-		Content      string                    `json:"content" validate:"required,max=4096"`
-		Metadata     map[string]any            `json:"metadata"`
-		Links        []models.NotificationLink `json:"links"`
-		IsForcePush  bool                      `json:"is_force_push"`
-		IsRealtime   bool                      `json:"is_realtime"`
-		UserID       uint                      `json:"user_id" validate:"required"`
+		Type        string                    `json:"type" validate:"required"`
+		Subject     string                    `json:"subject" validate:"required,max=1024"`
+		Content     string                    `json:"content" validate:"required,max=4096"`
+		Metadata    map[string]any            `json:"metadata"`
+		Links       []models.NotificationLink `json:"links"`
+		IsForcePush bool                      `json:"is_force_push"`
+		IsRealtime  bool                      `json:"is_realtime"`
 	}
 
 	if err := exts.BindAndValidate(c, &data); err != nil {
