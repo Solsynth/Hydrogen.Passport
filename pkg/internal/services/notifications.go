5e76fa07b7738662fa09f9fb9827b8fe9752357d
LittleSheep <littlesheep.code@hotmail.com>
2024-08-24 14:29:36 +0800

    :alien: Use hyper's NetworkPackage instead of own UnifiedCommand

--- pkg/internal/services/notifications.go
+++ pkg/internal/services/notifications.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"git.solsynth.dev/hydrogen/dealer/pkg/hyper"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/samber/lo"
 	"reflect"
@@ -71,7 +72,7 @@ func PushNotification(notification models.Notification) error {
 	defer cancel()
 	_, err := proto.NewStreamControllerClient(gap.H.GetDealerGrpcConn()).PushStream(ctx, &proto.PushStreamRequest{
 		UserId: lo.ToPtr(uint64(notification.AccountID)),
-		Body: models.UnifiedCommand{
+		Body: hyper.NetworkPackage{
 			Action:  "notifications.new",
 			Payload: notification,
 		}.Marshal(),
@@ -134,7 +135,7 @@ func PushNotificationBatch(notifications []models.Notification) {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		_, _ = stream.PushStream(ctx, &proto.PushStreamRequest{
 			UserId: lo.ToPtr(uint64(notification.AccountID)),
-			Body: models.UnifiedCommand{
+			Body: hyper.NetworkPackage{
 				Action:  "notifications.new",
 				Payload: notification,
 			}.Marshal(),
